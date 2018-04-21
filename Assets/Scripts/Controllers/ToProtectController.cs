2766d85b82c4f7d2abb7ef5a8a70d0a8db51dc7c
Frederic Filosa <filosa@applistic.com>
2018-04-21 17:29:53 +0900

    Added the "ToProtect" object.

---
+++ Assets/Scripts/Controllers/ToProtectController.cs
@@ -0,0 +1,31 @@
+﻿using Interfaces;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class ToProtectController : MonoBehaviour, IDamageable
+    {
+        // Properties
+        // ------------------------------------
+
+        public float health = 100.0f;
+    
+        // Public methods
+        // ------------------------------------
+    
+        public void ApplyDamage(float damage)
+        {
+            if (damage < 0)
+            {
+                return; // No healing this way.
+            }
+
+            health -= damage;
+
+            if (health <= 0)
+            {
+                // GameOver
+            }
+        }
+    }
+}
