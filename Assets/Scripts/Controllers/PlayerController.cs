0ec04d7f1746bd7ee5063f88e9193cd3ea00b332
Frederic Filosa <filosa@applistic.com>
2018-04-21 14:01:44 +0900

    Added physics to objects and a player controller.
    Unity's physics engine does not work well for 2d platformers in my opinion.
    The following tutorial presents a better approach:
    https://unity3d.com/learn/tutorials/topics/2d-game-creation/player-controller-script
    
    This is basically what I used, altering the code a little for better performance and readability.

---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Physics;
+using UnityEngine;
+
+namespace Controllers
+{
+    public class PlayerController : MovableObject
+    {
+        // Properties
+        // -----------------------------------
+
+        [Header("Movement")]
+        
+        [SerializeField] private float movementSpeed = 1f;
+        [SerializeField] private float jumpSpeed = 7f;
+        [SerializeField] private float jumpSlowDown = 0.5f;
+
+        private SpriteRenderer renderer;
+        
+        // Protected methods
+        // -----------------------------------
+
+        protected override void Start()
+        {
+            base.Start();
+
+            renderer = GetComponent<SpriteRenderer>();
+        }
+
+        protected override void ComputeVelocity()
+        {
+            base.ComputeVelocity();
+
+            targetVelocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, 0);
+            
+            // Modify velocity for jump.
+            if (grounded && Input.GetButtonDown("Jump"))
+            {
+                velocity.y = jumpSpeed;
+            }
+            else if (Input.GetButtonUp("Jump"))
+            {
+                if (velocity.y > 0)
+                {
+                    velocity.y *= jumpSlowDown;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Physics/MovableObject.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Physics
+{
+    public class MovableObject : MonoBehaviour
+    {
+        // Properties
+        // -----------------------------------
+
+        [Header("Physics")]
+        
+        public float gravityModifier = 1.0f;
+        public float minimumNormalY = 0.65f;
+        
+        protected Rigidbody2D rigidBody;
+        protected const float minimumMovementDistance = 0.001f;
+        protected const float shellRadius = 0.01f;
+        
+        protected Vector2 velocity;
+        protected Vector2 targetVelocity;
+        protected ContactFilter2D contactFilter;
+        protected RaycastHit2D[] collisionResults = new RaycastHit2D[16];
+        protected List<RaycastHit2D> collisions = new List<RaycastHit2D>(16);
+        protected bool grounded;
+        protected Vector2 groundNormal;
+
+        // Protected methods
+        // -----------------------------------
+        
+        protected void Move(Vector2 movement, bool yMovement)
+        {
+            float distance = movement.magnitude;
+
+            if (distance < minimumMovementDistance)
+            {
+                return;
+            }
+
+            int collisionsCount = rigidBody.Cast(movement, contactFilter, collisionResults, distance + shellRadius);
+
+            collisions.Clear();
+            for (int i = 0; i < collisionsCount; i++)
+            {
+                collisions.Add(collisionResults[i]);
+            }
+            
+            // Check normals.
+            // If the normal is more than minimumNormalY, we hit the ground. 
+            for (int i = 0; i < collisions.Count; i++)
+            {
+                Vector2 normal = collisions[i].normal;
+                if (normal.y > minimumNormalY)
+                {
+                    grounded = true;
+                    if (yMovement)
+                    {
+                        groundNormal = normal;
+                        normal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(velocity, normal);
+                if (projection < 0)
+                {
+                    velocity = velocity - (projection * normal);
+                }
+
+                float modifiedDistance = collisions[i].distance - shellRadius;
+                if (modifiedDistance < distance)
+                {
+                    distance = modifiedDistance;
+                }
+            }
+            
+            rigidBody.position += movement.normalized * distance;
+        }
+
+        protected virtual void ComputeVelocity()
+        {
+            
+        }
+        
+        protected virtual void Start()
+        {
+            contactFilter.useTriggers = false;
+            contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+            // TODO: Setup Physics2D's collision matrix.
+        }
+        
+        // Private methods
+        // -----------------------------------
+
+        private void OnEnable()
+        {
+            rigidBody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            targetVelocity = Vector2.zero;
+            ComputeVelocity();
+        }
+
+        private void FixedUpdate()
+        {
+            velocity += Physics2D.gravity * gravityModifier * Time.deltaTime;
+            velocity.x = targetVelocity.x;
+            
+            grounded = false;
+
+            Vector2 deltaPosition = velocity * Time.deltaTime;
+
+            Vector2 moveAlongGround = new Vector2(groundNormal.y, -groundNormal.x);
+            Vector2 movement = moveAlongGround * deltaPosition.x;
+            Move(movement, false);
+            
+            movement = Vector2.up * deltaPosition.y;
+            Move(movement, true);
+        }
+    }
+}
