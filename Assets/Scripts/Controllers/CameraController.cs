4e5270d2024e7bf45ef0a74c052511c8c97ce6dd
Frederic Filosa <filosa@applistic.com>
2018-04-21 13:58:15 +0900

    Refactoring and updated the camera controller so that it follows the player.
    Very basic implementation for the camera controller but it will work for now.
    If there is time, I'll add a more "2d platformer" controller that only
    follows the player when the player moved a specified distance.

---
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Controllers
+{
+    public class CameraController : MonoBehaviour
+    {
+        // Properties
+        // ----------------------------------
+
+        public GameObject target;
+        
+        // Private methods
+        // ----------------------------------
+        
+        private void LateUpdate()
+        {
+            if (target == null)
+            {
+                return;
+            }        
+            
+            transform.position = target.transform.position;
+        }
+    }
+}
