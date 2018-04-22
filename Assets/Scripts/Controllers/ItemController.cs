130db4e69a9f0c6e98f550636086efee4f2f4f81
Frederic Filosa <filosa@applistic.com>
2018-04-22 18:40:51 +0900

    Added Heart item that restore health (not done yet).

---
+++ Assets/Scripts/Controllers/ItemController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Controllers
+{
+    public class ItemController : MonoBehaviour
+    {
+        // Properties
+        // -----------------------------------
+
+        private Animator animator;
+        
+        // Public methods
+        // -----------------------------------
+
+        public void OnPickupAnimationEnded()
+        {
+            Destroy(gameObject);
+        }
+        
+        // Private methods
+        // -----------------------------------
+        
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.gameObject.CompareTag("Player"))
+            {
+                return;
+            }
+            
+            animator.Play("Pickup");
+        }
+    }
+}
